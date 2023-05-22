52c9cbaaaff18c6a63d08f3a0898b421e1a95247
TekniqueJoseph <joejacksun@gmail.com>
2023-05-22 10:11:52 -0400

    cleaned code

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-const open = document.getElementById('responsivelist');
+const slide = document.getElementById('responsivelist');
 const closeL = document.querySelectorAll('.li')
 const closeSlide = document.getElementById('closemenu')
 const newMenu = document.getElementById('hamburger')
 
 const openMenu = () => {
-    open.style.width = "200px";
+    slide.style.width = "200px";
     return
 }
 
 const closeMenu = () => {
-    open.style.width = "0";
+    slide.style.width = "0";
     return
 }
 
 closeL.forEach(list => {
     list.addEventListener('click', function handleClick(event) {
-      console.log('box clicked', event);
-      open.style.width = "0";
+      slide.style.width = "0";
     });
   }); 
 newMenu.addEventListener('click', openMenu)
