0e3c54822fd3767bcf180eafdc50a8070081cfcb
TekniqueJoseph <joejacksun@gmail.com>
2023-05-21 22:22:14 -0400

    landing page complete

--- script.js
+++ script.js
@@ -3,16 +3,13 @@ const closeL = document.querySelectorAll('.li')
 const closeSlide = document.getElementById('closemenu')
 const newMenu = document.getElementById('hamburger')
 
-
 const openMenu = () => {
     open.style.width = "200px";
     return
 }
 
 const closeMenu = () => {
-    console.log('Hello')
     open.style.width = "0";
-    
     return
 }
 
@@ -20,18 +17,8 @@ closeL.forEach(list => {
     list.addEventListener('click', function handleClick(event) {
       console.log('box clicked', event);
       open.style.width = "0";
-  
-    //   .setAttribute('style', 'background-color: yellow;');
     });
-  });
-
-
-// const liCloseMenu = () => {
-//     // open.style.width = "0";
-//     console.log('Hello')
-//     return
-// }
- 
+  }); 
 newMenu.addEventListener('click', openMenu)
 closeSlide.addEventListener('click', closeMenu)
-// closeL.addEventListener('click', closeS)
+
